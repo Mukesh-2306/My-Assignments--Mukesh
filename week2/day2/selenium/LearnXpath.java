e1e2f9dc22a521cdeb4ed3da88963d1d8c4c24a1
Mukesh-2306 <158569869+Mukesh-2306@users.noreply.github.com>
2024-02-04 15:23:41 +0530

    Add files via upload
    week2day4

---
+++ week2/day2/selenium/LearnXpath.java
@@ -0,0 +1,24 @@
+package learn.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnXpath {
+
+	public static void main(String[] args) {
+		// Setup the driver and launch
+				ChromeDriver driver = new ChromeDriver();
+				
+				//Load the url
+		        driver.get("http://leaftaps.com/opentaps/control/main");
+		        driver.manage().window().maximize();
+		        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("demosalesmanager");
+		        driver.findElement(By.xpath("//input[@name='PASSWORD']")).sendKeys("crmsfa");
+		        driver.findElement(By.xpath("//input[@value='Login']")).click();
+		        driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+		        driver.findElement(By.linkText("Leads")).click();
+		        driver.findElement(By.linkText("Create Lead")).click();
+
+	}
+
+}
